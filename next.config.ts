f51c6ea18c7736006ff938db57bee006a64ae693
Damir <damiratamuratov07@gmail.com>
2026-02-08 04:15:56 +0500

    switched from yarn to npm

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,9 @@
 import type { NextConfig } from 'next'
-import path from 'path'
 
 const nextConfig: NextConfig = {
 	turbopack: {
-		root: path.join(__dirname, '..'),
+		root: __dirname,
 	},
 }
-
+console.log(__dirname)
 export default nextConfig
